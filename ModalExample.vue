80189f3a7f4195a1a5dc621cbafcc323174dbdbe
JonasTks <71845648+JonasTks@users.noreply.github.com>
2020-09-25 09:20:16 +0300

    Add files via upload

---
+++ ModalExample.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <button class="button is-warning" @click="isActive=true">Click me</button>
+  <div class="modal" :class="{'is-active' : isActive}">
+  <div class="modal-background" @click="isActive=false"></div>
+  <div class="modal-content">
+    <div class="card">
+        <img src="https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg" height="1920" width="1080">
+     </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            isActive: false
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ example.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <i>{{message}}</i>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['message']
+}
+</script>
